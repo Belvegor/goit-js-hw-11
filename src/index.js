dc20f8e341cb632f31a0ebaeb062002f77bfc004
Igor <igor_socha@yahoo.com>
2023-12-13 12:51:22 +0100

    v.1.1

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 const apiKey = '41214724-8e0af22b522780ea083dbfd83'; 
 let currentPage = 1;
 let searchQuery = '';
+const perPage = 40;
 
 const form = document.getElementById('search-form');
 const gallery = document.querySelector('.gallery');
@@ -25,7 +26,7 @@ loadMoreBtn.addEventListener('click', function() {
 });
 
 async function searchImages() {
-  const response = await fetch(`https://pixabay.com/api/?key=${apiKey}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${currentPage}&per_page=40`);
+  const response = await fetch(`https://pixabay.com/api/?key=${apiKey}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${currentPage}&per_page=${perPage}`);
   const data = await response.json();
 
   if (data.hits.length === 0) {
@@ -55,31 +56,24 @@ async function searchImages() {
     gallery.appendChild(card);
   });
 
-  loadMoreBtn.style.display = 'block';
+  const totalDisplayed = currentPage * perPage;
 
-  
+  if (totalDisplayed >= data.totalHits) {
+    loadMoreBtn.style.display = 'none';
+  } else {
+    loadMoreBtn.style.display = 'block';
+  }
+
   const lightbox = new SimpleLightbox('.lightbox', {
     elements: '.photo-card a'
   });
 
-  
-  const lightboxOverlay = document.querySelector('.sl-overlay');
-  const lightboxImage = document.querySelector('.sl-image');
-
-  lightboxOverlay.style.backgroundColor = 'rgba(0, 0, 0, 0.85)';
-  lightboxImage.style.maxWidth = '90%';
-  lightboxImage.style.maxHeight = '90%';
-  lightboxImage.style.marginTop = '5vh';
-
   lightbox.refresh();
 
-  
   const { height: cardHeight } = gallery.firstElementChild.getBoundingClientRect();
   window.scrollBy({ top: cardHeight * 2, behavior: 'smooth' });
 
-  
-  if (data.totalHits <= currentPage * 40) {
-    loadMoreBtn.style.display = 'none';
+  if (totalDisplayed >= data.totalHits) {
     Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
   }
 }
